c295044d5095515465e54cd14b4f84f4bccdad5c
Pieter <Pieter.de.bie@planet.nl>
2015-05-19 18:16:33 +0200

    This is a working example. Now I am going to change the variable names and write the documentation.

--- cachevector.R
+++ cachevector.R
@@ -1,32 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
- makeCacheMatrix <- function(x = matrix()) {
+makeVector <- function(x = numeric()) {
         m <- NULL
         set <- function(y) {
-                z <<- y
+                x <<- y
                 m <<- NULL
         }
         get <- function() x
-        setmean <- function(solve) m <<- solve
+        setmean <- function(mean) m <<- mean
         getmean <- function() m
         list(set = set, get = get,
              setmean = setmean,
              getmean = getmean)
 }
 
- ## Write a short comment describing this function
- 
- cacheSolve <- function(x, ...) {
+cachemean <- function(x, ...) {
         m <- x$getmean()
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
         data <- x$get()
-        m <- solve(data, ...)
+        m <- mean(data, ...)
         x$setmean(m)
         m
 }
